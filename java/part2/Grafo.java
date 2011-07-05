cacb5cd79110b95237d27a34f838f5ff53b7b889
mastergreg <gregliras@gmail.com>
2011-07-06 02:50:38 +0300

    removed comments

--- java/part2/Grafo.java
+++ java/part2/Grafo.java
@@ -4,15 +4,6 @@ public class Grafo
 
 	public static void main(String[] args)
 	{
-    /*fileParser test = new fileParser();
-    test.parseIn();
-    //test.printNodeList();
-    Tree myTestTree = new Tree();
-    myTestTree.insertNodesInTree(test.getNodeList()); 
-    //test.printNodeList();
-    TreeParser Tparser = new TreeParser(myTestTree.getHeadList());
-    Tparser.parseTree();
-    */
     ArrayFileParser boom = new ArrayFileParser();
     boom.parseIn();
     int[][] tick = boom.getNodeArray();
